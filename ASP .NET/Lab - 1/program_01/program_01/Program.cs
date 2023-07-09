2cb78383f03bd0e29b949c4df66b2fb6f21d8514
Divyank Raja <107948268+RajaDivyank@users.noreply.github.com>
2023-07-09 12:30:53 +0530

    .NET

---
+++ ASP .NET/Lab - 1/program_01/program_01/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Write a program to print your name, address, contact number & city.*/
+using System;
+namespace program_01
+{
+    class Program { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name : Divyank Raja");
+            Console.WriteLine("Address : bhagwati krupa, vaniyawadi street number 7,near shyamwadi rajkot");
+            Console.WriteLine("Contact Number : 1234567890");
+            Console.WriteLine("City : Raajkot");
+        }
+}
+}
